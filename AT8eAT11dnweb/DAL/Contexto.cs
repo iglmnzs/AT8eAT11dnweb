776ddcc31506230a026067ad577ed8359f3b362d
igorcdemenezes <igorcdemenezes@gmail.com>
2024-09-24 10:13:38 -0300

    Add project files.

---
+++ AT8eAT11dnweb/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using AT8eAT11dnweb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AT8eAT11dnweb.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ AT8eAT11dnweb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AT8eAT11dnweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
